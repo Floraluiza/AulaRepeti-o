71dc6e82396a708cee71bb6a1a7c41106648a6c9
florasantana <39812840+Floraluiza@users.noreply.github.com>
2021-09-23 22:34:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// enquanto (while)
+
+
+/*
+INCREMENTO
+var cont = 10
+
+  while(cont <= 10){
+  //caso cindição a continudiade seja verdadeira.
+
+  alert('Valor do contador: ' + cont)
+  //cont++
+
+  cont = cont + 2
+
+}
+*/
+
+/*DECREMENTO
+var cont = 10
+
+while(cont >= 0){
+  //caso cindição a continudiade seja verdadeira.
+
+  alert('Valor do contador: ' + cont)
+  //cont--
+
+  cont = cont - 1
+
+}
+*/
+
+
+//repita até (do-while)
+
+var cont = 11
+
+do{
+
+  alert('Valor do contador: ' + cont)
+  //cont++
+
+  cont = cont + 2
+  
+}while(cont < 10)
+
+
+
+
+
+
